28d29b3b7da2d370f87194ea6617110a75d4e344
Flávio Dinis de Oliveira Figueiredo Lopes da Silva <dinissilvauk@gmail.com>
2022-07-18 15:15:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const dataFile = "kepler_data.csv";
+
+const habitablePlanets = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+fs.createReadStream(dataFile)
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(`${habitablePlanets.length} habitable planets found!`);
+  });
